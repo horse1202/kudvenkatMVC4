ef4d1aee0f2e9489d59868eb9e8752fb04ce62c4
horse1202 <129653125+horse1202@users.noreply.github.com>
2023-04-02 13:35:35 +0500

    Initial commit

---
+++ MVC2Application/MVC2Application/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC2Application.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
